00c95e148bf2b48ef548a32a806373cd55dc4f62
yogitasehgal6629@gmail.com <yogitasehgal6629@gmail.com>
2025-02-22 15:19:46 +0530

    initial commit

---
+++ test/features/demofeatures/demo.feature
@@ -0,0 +1,11 @@
+Feature: Demo feature
+@demo
+    Scenario Outline: Run first demo feature
+        Given Google page is opened
+        When Search with <SearchItem>
+        Then Click on first search result
+        Then The URL should match with <expectedURL>
+        Examples:
+            | testID  | SearchItem | expectedURL |
+            | Demo_TC001 | WDIO  | https://webdriver.io/  |
+
---
+++ test/features/step-definitions/demo.ts
@@ -0,0 +1,33 @@
+import { Given, When, Then } from "@wdio/cucumber-framework";
+import * as chai from "chai"
+
+Given(/^Google page is opened$/, async function(){
+    await browser.url("https://www.google.com/")
+   
+})
+
+When(/^Search with (.*)$/, async function(SearchItem){
+    // console.log(`>>searchItem: ${SearchItem} `)
+    let searchBox=  $(`[name="q"]`)
+    await searchBox.setValue(SearchItem)
+    let googlesearch=$('[value="Google Search"]')
+    await googlesearch.click()
+    await browser.keys('Enter')
+
+
+    })
+
+Then(/^Click on first search result$/,async function(){
+await browser.pause(10000)   
+let element= $(`<h3>`)
+await element.click()
+
+})
+
+Then(/^The URL should match with (.*)$/,async function(expectedURL){
+    console.log(`expected URL: ${expectedURL}`);
+    let actualUrl=await browser.getUrl()
+    console.log(actualUrl);
+    chai.expect(actualUrl).to.equal(expectedURL)
+
+})
